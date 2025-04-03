c08675158a578e84ddfac16c82f34288a9a1d75a
Danilo Giannone <danilogiannone00@gmail.com>
2025-04-03 12:42:38 +0200

    add button on the calculator fix the visuals and the UI

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
+//the order is the order of apparition row by row
+const keyboardText=[["ac","del","+/-","/"],
+                 ["7","8","9","*"],
+                 ["4","5","6","-"],
+                 ["1","2","3","+"],
+                 ["0",".","=","%"]];
+
 let container = document.querySelector("#container");
+let keyboard = document.querySelector(".keyboard");
+for (let i = 0; i < keyboardText.length; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    keyboard.appendChild(row);
+    for (let j = 0; j < keyboardText[i].length; j++) {
+        let button = document.createElement("button");
+        button.classList.add("btn");
+        button.textContent = keyboardText[i][j];
+        row.appendChild(button);  
+    }
+    
+}
 window.onload = refresh();
 window.addEventListener('resize', function(event){
     refresh();
