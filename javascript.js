fa213e4cb02a590dd293c77262f2c80e1c3c5dcf
Danilo Giannone <danilogiannone00@gmail.com>
2025-04-03 13:15:10 +0200

    add basic check for the display and a simple error handler

--- javascript.js
+++ javascript.js
@@ -7,6 +7,7 @@ const keyboardText=[["ac","del","+/-","/"],
 
 let container = document.querySelector("#container");
 let keyboard = document.querySelector(".keyboard");
+let display = document.querySelector(".display");
 for (let i = 0; i < keyboardText.length; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
@@ -15,9 +16,15 @@ for (let i = 0; i < keyboardText.length; i++) {
         let button = document.createElement("button");
         button.classList.add("btn");
         button.textContent = keyboardText[i][j];
+        for (let z = 0; z < 10; z++) {
+            if(keyboardText[i][j] == z.toString()){
+               button.classList.add("digit");              
+            }else{
+                button.classList.add("operator");
+            }   
+        }
         row.appendChild(button);  
     }
-    
 }
 window.onload = refresh();
 window.addEventListener('resize', function(event){
@@ -30,3 +37,26 @@ function refresh(){
         container.setAttribute("style", "height:70vw; width: 50vw")
     }
 };
+let operatorFlag = false;
+let firstNumber = "";
+let SecondNumber;
+let TotalNumber;
+let operator;
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+    button.addEventListener("click", (e)=>{
+        if(!operatorFlag){
+            firstNumber += (e.target.textContent)
+            checkNumber(firstNumber)
+            
+            
+        }
+    })
+});
+function checkNumber(num){
+    if(num.length <= 21){
+        display.textContent = num;
+    }else{
+        display.textContent = "Error! Too many digits!"
+    }
+}
